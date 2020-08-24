b59a3b058371e152e0978827e27dd7d6e0bb370d
Alexander von Below <alexander.von_below@telekom.de>
2020-08-24 12:30:09 +0200

    Initial Commit of Sourcecode

---
+++ HelloActions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloActions
+//
+//  Created by Alexander v. Below on 24.08.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
